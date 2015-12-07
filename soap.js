a02a5654377e63b2da0b136bfd30db4ee0860f11
cagdas.yigit <cagdas.yigit@pia-team.com>
2015-12-07 14:51:37 +0200

    Initial Commit

---
+++ soap.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var PrmWebApp = angular.module('PrmWebApp');
+
+PrmWebApp.register.factory("$soap", ['$q', '$rootScope', function ($q, $rootScope) {
+	// Changes XML to JSON
+	var xmlToJson = function (xml) {
+		
+		// Create the return object
+		var obj = {};
+
+		if (xml.nodeType == 1) { // element
+			// do attributes
+			if (xml.attributes.length > 0) {
+			obj["@attributes"] = {};
+				for (var j = 0; j < xml.attributes.length; j++) {
+					var attribute = xml.attributes.item(j);
+					obj["@attributes"][attribute.nodeName.replace(":", "")] = attribute.nodeValue;
+				}
+			}
+		} else if (xml.nodeType == 3) { // text
+			obj = xml.nodeValue;
+		}
+
+		// do children
+		if (xml.hasChildNodes()) {
+			for(var i = 0; i < xml.childNodes.length; i++) {
+				var item = xml.childNodes.item(i);
+				var nodeName = item.nodeName.replace(":", "");
+				if (typeof(obj[nodeName]) == "undefined") {
+					obj[nodeName] = xmlToJson(item);
+				} else {
+					if (typeof(obj[nodeName].push) == "undefined") {
+						var old = obj[nodeName];
+						obj[nodeName] = [];
+						obj[nodeName].push(old);
+					}
+					obj[nodeName].push(xmlToJson(item));
+				}
+			}
+		}
+		return obj;
+	};
+
+	return {
+		sendTxdr: function(params){
+			var deferred = $q.defer();
+			
+			//Create SOAPClientParameters
+			var soapParams = new SOAPClientParameters();
+			for(var param in params){
+				soapParams.add(param, params[param]);
+			}
+			
+			//Create Callback
+			var soapCallback = function(o, e){
+				if(e.constructor.toString().indexOf("function Error()") != -1){
+					deferred.reject("An error has occurred.");
+				} else {
+					deferred.resolve(xmlToJson(e).envEnvelope.envBody.ExecuteResponse);
+				}
+			}
+			
+			SOAPClient.invoke($rootScope.soapURL, soapParams, true, soapCallback);
+
+			return deferred.promise;
+		},
+		setCredentials: function(username, password){
+			SOAPClient.username = username;
+			SOAPClient.password = password;
+		}
+	}
+}]);
